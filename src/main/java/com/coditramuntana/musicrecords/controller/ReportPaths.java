85fd6dee418fce83bee47d941fcf2ad0cf639a73
Joel Hernandez Pla <joelpla1@hotmail.com>
2026-09-09 20:57:34 +0200

    feat: add discography report endpoint

---
+++ src/main/java/com/coditramuntana/musicrecords/controller/ReportPaths.java
@@ -0,0 +1,16 @@
+package com.coditramuntana.musicrecords.controller;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+/**
+ * Route constants of the reporting API.
+ */
+@NoArgsConstructor(access = PRIVATE)
+public final class ReportPaths {
+
+    public static final String BASE_PATH = "/api/report";
+    public static final String DISCOGRAPHY_PATH = "/discography";
+
+}
